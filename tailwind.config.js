dca1ecfa835ac8242e437b5a8bd0dc94668835ca
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-09-08 15:28:44 +0500

    add Fixed hero header top errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,18 @@ module.exports = {
       fontSize:{
         '42':'42px'
       },
+      backgroundImage:{
+        'img1':"url('/img/Group-188.png')",
+        'img2':"url('/img/Group-189.svg')",
+        'img3':"url('/img/Group-190.png')",
+        'img4':"url('/img/Group-191.png')",
+        'img5':"url('/img/Group-192.png')",
+        'img6':"url('/img/Group-193.png')",
+      },
+      gap:{
+        '30':'30px'
+      } 
+      
     },
   },
   plugins: [],
